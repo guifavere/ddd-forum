4d268e98f0cb563db342eebf252867f3a98c2626
guifavere <guilherme.favere@gmail.com>
2021-04-02 18:12:09 -0300

    Update domain model: User

--- src/modules/users/domain/user.ts
+++ src/modules/users/domain/user.ts
@@ -4,6 +4,8 @@ import { UserId } from './userId';
 import { UserPassword } from './userPassword';
 import { JWTToken, RefreshToken } from './jwt';
 import { UserCreated } from './events/userCreated';
+import { UserDeleted } from './events/userDeleted';
+import { UserLoggedIn } from './events/userLoggedIn';
 import { UniqueEntityID } from '../../../shared/domain/UniqueEntityID';
 import { Result } from '../../../shared/core/Result';
 import { Guard } from '../../../shared/core/Guard';
@@ -22,9 +24,67 @@ interface UserProps {
 }
 
 export class User extends AggregateRoot<UserProps> {
-
   private constructor(props: UserProps, id?: UniqueEntityID) {
     super(props, id);
+  }
+
+  get userId(): UserId {
+    return UserId.create(this._id).getValue();
+  }
+
+  get email(): UserEmail {
+    return this.props.email;
+  }
+
+  get username(): UserName {
+    return this.props.username;
+  }
+
+  get password(): UserPassword {
+    return this.props.password;
+  }
+
+  get accessToken(): string {
+    return this.props.accessToken;
+  }
+
+  get isDeleted(): boolean {
+    return this.props.isDeleted;
+  }
+
+  get isEmailVerified(): boolean {
+    return this.props.isEmailVerified;
+  }
+
+  get isAdminUser(): boolean {
+    return this.props.isAdminUser;
+  }
+
+  get lastLogin(): Date {
+    return this.props.lastLogin;
+  }
+
+  get refreshToken(): RefreshToken {
+    return this.props.refreshToken;
+  }
+
+  public isLoggedIn(): boolean {
+    return !!this.props.accessToken && !!this.props.refreshToken;
+  }
+
+  public setAccessToken(token: JWTToken, refreshToken: RefreshToken): void {
+    this.addDomainEvent(new UserLoggedIn(this));
+
+    this.props.accessToken = token;
+    this.props.refreshToken = refreshToken;
+    this.props.lastLogin = new Date();
+  }
+
+  public delete(): void {
+    if (!this.props.isDeleted) {
+      this.addDomainEvent(new UserDeleted(this));
+      this.props.isDeleted = true;
+    }
   }
 
   public static create(props: UserProps, id?: UniqueEntityID): Result<User> {
@@ -51,21 +111,5 @@ export class User extends AggregateRoot<UserProps> {
     }
 
     return Result.ok<User>(user);
-  }
-
-  get userId(): UserId {
-    return UserId.create(this._id).getValue();
-  }
-
-  get email(): UserEmail {
-    return this.props.email;
-  }
-
-  get username(): UserName {
-    return this.props.username;
-  }
-
-  get password(): UserPassword {
-    return this.props.password;
   }
 }
