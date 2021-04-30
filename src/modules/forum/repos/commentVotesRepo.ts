ac02ad7acc6fec0d25a168050b4ec0427f09280e
guifavere <guilherme.favere@gmail.com>
2021-04-29 21:03:13 -0300

    Add repository classes context forum

---
+++ src/modules/forum/repos/commentVotesRepo.ts
@@ -0,0 +1,20 @@
+import { CommentVote } from '../domain/commentVote';
+import { MemberId } from '../domain/memberId';
+import { CommentId } from '../domain/commentId';
+import { CommentVotes } from '../domain/commentVotes';
+import { VoteType } from '../domain/vote';
+import { PostId } from '../domain/postId';
+
+export interface ICommentVotesRepo {
+  exists(commentId: CommentId, memberId: MemberId, voteType: VoteType): Promise<boolean>;
+  getVotesForCommentByMemberId(commentId: CommentId, memberId: MemberId): Promise<CommentVote[]>;
+  countUpvotesForCommentByCommentId(commentId: CommentId): Promise<number>;
+  countDownvotesForCommentByCommentId(commentId: CommentId): Promise<number>;
+  countAllPostCommentUpvotes(postId: PostId): Promise<number>;
+  countAllPostCommentDownvotes(postId: PostId): Promise<number>;
+  countAllPostCommentUpvotesExcludingOP(postId: PostId): Promise<number>;
+  countAllPostCommentDownvotesExcludingOP(postId: PostId): Promise<number>;
+  saveBulk(votes: CommentVotes): Promise<any>;
+  save(vote: CommentVote): Promise<any>;
+  delete(vote: CommentVote): Promise<any>;
+}
