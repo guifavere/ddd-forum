a461fb6731c204e25fb7337b726e07f540a10f02
guifavere <guilherme.favere@gmail.com>
2021-04-29 21:03:54 -0300

    Add dto classes forum context

---
+++ src/modules/forum/dtos/commentDTO.ts
@@ -0,0 +1,15 @@
+import { MemberDTO } from './memberDTO';
+
+export interface CommentDTO {
+  postSlug: string;
+  postTitle: string;
+  commentId: string;
+  parentCommentId?: string;
+  text: string;
+  member: MemberDTO;
+  createdAt: string | Date;
+  childComments: CommentDTO[];
+  points: number;
+  wasUpvotedByMe: boolean;
+  wasDownvotedByMe: boolean;
+}
---
+++ src/modules/forum/dtos/postDTO.ts
@@ -0,0 +1,16 @@
+import { MemberDTO } from './memberDTO';
+import { PostType } from '../domain/postType';
+
+export interface PostDTO {
+  slug: string;
+  title: string;
+  createdAt: string | Date;
+  memberPostedBy: MemberDTO;
+  numComments: number;
+  points: number;
+  text: string;
+  link: string;
+  type: PostType;
+  wasUpvotedByMe: boolean;
+  wasDownvotedByMe: boolean;
+}
