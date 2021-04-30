81417bf4dd76b1c43e80a1c2f74a5e5ce4a19db2
guifavere <guilherme.favere@gmail.com>
2021-04-29 21:06:25 -0300

    Add useCase class that create member

---
+++ src/modules/forum/useCases/members/createMember/CreateMemberDTO.ts
@@ -0,0 +1,3 @@
+export interface CreateMemberDTO {
+  userId: string;
+}
---
+++ src/modules/forum/useCases/members/createMember/index.ts
@@ -0,0 +1,7 @@
+import { CreateMember } from './CreateMember';
+import { userRepo } from '../../../../users/repos';
+import { memberRepo } from '../../../repos';
+
+const createMember = new CreateMember(userRepo, memberRepo);
+
+export { createMember };
