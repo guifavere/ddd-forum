68d9144e5c0f365910bf88a9b0a8806437b3e34d
guifavere <guilherme.favere@gmail.com>
2021-04-29 21:07:31 -0300

    Add forum class subscribes user created event

---
+++ src/modules/forum/subscriptions/afterUserCreated.ts
@@ -0,0 +1,29 @@
+import { UserCreated } from '../../users/domain/events/userCreated';
+import { IHandle } from '../../../shared/domain/events/IHandle';
+import { CreateMember } from '../useCases/members/createMember/CreateMember';
+import { DomainEvents } from '../../../shared/domain/events/DomainEvents';
+
+export class AfterUserCreated implements IHandle<UserCreated> {
+  private createMember: CreateMember;
+
+  constructor(createMember: CreateMember) {
+    this.setupSubscriptions();
+    this.createMember = createMember;
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(this.onUserCreated.bind(this), UserCreated.name);
+  }
+
+  private async onUserCreated(event: UserCreated): Promise<void> {
+    const { user } = event;
+
+    try {
+      await this.createMember.execute({ userId: user.userId.id.toString() });
+
+      console.log(`[AfterUserCreated]: Successfully executed CreateMember use case AfterUserCreated`);
+    } catch (error) {
+      console.log(`[AfterUserCreated]: Failed to execute CreateMember use case AfterUserCreated.`);
+    }
+  }
+}
